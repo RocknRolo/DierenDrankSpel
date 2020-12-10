cfbe3968f2ff1abbf331a7a4d9a031cea1aaae9d
Rock'n'Rolo <42418205+RocknRolo@users.noreply.github.com>
2020-12-10 16:52:20 +0100

    onnodige vars verwijderd

--- game.js
+++ game.js
@@ -1,27 +1,17 @@
 let TIME_LIMIT = 10;
 
-
 let wordList = readTextFile();
 // console.log(wordList);
 
 let timer_text = document.querySelector(".curr_time");
-let error_text = document.querySelector(".curr_errors");
-let cpm_text = document.querySelector(".curr_cpm");
-let wpm_text = document.querySelector(".curr_wpm");
-let quote_text = document.querySelector(".quote");
 let input_area = document.querySelector(".input_area");
-let restart_btn = document.querySelector(".restart_btn");
-let cpm_group = document.querySelector(".cpm");
-let wpm_group = document.querySelector(".wpm");
-let error_group = document.querySelector(".errors");
+
+let letter = document.querySelector(".letter");
 
 let timeLeft = TIME_LIMIT;
-let timeElapsed = 0;
-let total_errors = 0;
-let errors = 0;
+let letter = "";
 let characterTyped = 0;
-let current_quote = "";
-let quoteNo = 0;
+let current_word = "";
 let timer = null;
 
 function updateQuote() {
@@ -167,6 +157,5 @@ function resetValues() {
 }
 
 function readTextFile() {
-
 
 }
