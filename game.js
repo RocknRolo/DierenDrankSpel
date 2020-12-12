9d5040019f64c29631f8f5a4d717cedd5e0d5d7f
Rock'n'Rolo <42418205+RocknRolo@users.noreply.github.com>
2020-12-12 23:55:13 +0100

    update 3
    als het goed is, is er nu weinig meer van de oorspronkelijke code over. ik heb wat dingen hernoemd en wat geexperimenteerd met de drink_banner.
    TODO:
    list.txt inlezen als stringarray
    Timer werkt nog niet goed
    drinkbanner als overlay (flitsen?)

--- game.js
+++ game.js
@@ -1,161 +1,67 @@
 let TIME_LIMIT = 10;
 
-let wordList = readTextFile();
-// console.log(wordList);
+// deze stringarray is voor testdoeleinden totdat ik snap hoe ik 'n
+// textbestand (csv xml json xml whatever) kan uitlezen.
+let wordList = ["hond", "vis", "slang", "giraffe", "egel", "slak", 
+"krekel", "lijster", "reiger", "rendier", "regenworm", "paard", "kat", 
+"tijger", "reebok","krokodil", "leeuw", "walvis", "schildpad", "dodo", "olifant", "teek"];
 
-let timer_text = document.querySelector(".curr_time");
-let input_area = document.querySelector(".input_area");
+let namedWords = [];
+//console.log(wordList);
 
 let letter = document.querySelector(".letter");
+let timer = document.querySelector(".timer_text");
+let input_area = document.querySelector(".input_area");
+let drink_banner = document.querySelector(".drink_banner");
 
 let timeLeft = TIME_LIMIT;
-let letter = "";
-let characterTyped = 0;
 let current_word = "";
-let timer = null;
-
-function updateQuote() {
-  quote_text.textContent = null;
-  current_quote = quotes_array[quoteNo];
-
-  // separate each character and make an element
-  // out of each of them to individually style them
-  current_quote.split('').forEach(char => {
-    const charSpan = document.createElement('span')
-    charSpan.innerText = char
-    quote_text.appendChild(charSpan)
-  })
-
-  // roll over to the first quote
-  if (quoteNo < quotes_array.length - 1)
-    quoteNo++;
-  else
-    quoteNo = 0;
-}
 
 function processCurrentText() {
 
-  // get current input text and split it
-  curr_input = input_area.value;
-  curr_input_array = curr_input.split('');
-
-  // increment total characters typed
-  characterTyped++;
-
-  errors = 0;
-
-  quoteSpanArray = quote_text.querySelectorAll('span');
-  quoteSpanArray.forEach((char, index) => {
-    let typedChar = curr_input_array[index]
-
-    // characters not currently typed
-    if (typedChar == null) {
-      char.classList.remove('correct_char');
-      char.classList.remove('incorrect_char');
-
-      // correct characters
-    } else if (typedChar === char.innerText) {
-      char.classList.add('correct_char');
-      char.classList.remove('incorrect_char');
-
-      // incorrect characters
-    } else {
-      char.classList.add('incorrect_char');
-      char.classList.remove('correct_char');
-
-      // increment number of errors
-      errors++;
+    curr_input = input_area.value;
+    if (wordList.includes(curr_input)) {
+        current_word = curr_input;
+        curr_input = "";
+        resetValues;
     }
-})
-
-  // display the number of errors
-  error_text.textContent = total_errors + errors;
-
-  // if current text is completely typed
-  // irrespective of errors
-  if (curr_input.length == current_quote.length) {
-    updateQuote();
-
-    // update total errors
-    total_errors += errors;
-
-    // clear the input area
-    input_area.value = "";
-  }
 }
 
 function updateTimer() {
-  if (timeLeft > 0) {
-    // decrease the current time left
-    timeLeft--;
-
-    // increase the time elapsed
-    timeElapsed++;
-
-    // update the timer text
-    timer_text.textContent = timeLeft + "s";
-  }
-  else {
-    // finish the game
-    finishGame();
-  }
+    if (timeLeft > 0) {
+        timeLeft--;
+        timer.textContent = timeLeft + "s";
+    }
+    else {
+        drink();
+    }
 }
-
-function finishGame() {
-  // stop the timer
-  clearInterval(timer);
-
-  // disable the input area
-  input_area.disabled = true;
-
-  // show finishing text
-  quote_text.textContent = "Click on restart to start a new game.";
-
-  // display restart button
-  restart_btn.style.display = "block";
-
-  // calculate cpm and wpm
-  cpm = Math.round(((characterTyped / timeElapsed) * 60));
-  wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60));
-
-  // update cpm and wpm text
-  cpm_text.textContent = cpm;
-  wpm_text.textContent = wpm;
-
-  // display the cpm and wpm
-  cpm_group.style.display = "block";
-  wpm_group.style.display = "block";
-}
-
 
 function startGame() {
 
-  resetValues();
-  updateQuote();
+    resetValues();
 
-  // clear old and start a new timer
-  clearInterval(timer);
-  timer = setInterval(updateTimer, 1000);
+    clearInterval(timer);
+    timer_text = setInterval(updateTimer, 1000);
 }
 
 function resetValues() {
-  timeLeft = TIME_LIMIT;
-  timeElapsed = 0;
-  errors = 0;
-  total_errors = 0;
-  characterTyped = 0;
-  quoteNo = 0;
-  input_area.disabled = false;
+    clearInterval(timer);
 
-  input_area.value = "";
-  quote_text.textContent = 'Click on the area below to start the game.';
-  timer_text.textContent = timeLeft + 's';
-  error_text.textContent = 0;
-  restart_btn.style.display = "none";
-  cpm_group.style.display = "none";
-  wpm_group.style.display = "none";
+    timeLeft = TIME_LIMIT;
+    
+    input_area.value = "";
+    timer.textContent = timeLeft + 's';
 }
 
-function readTextFile() {
+function drink() {
+    drink_banner.style.display = "block";
 
 }
+
+/*
+TODO:
+list.txt inlezen als stringarray
+Timer werkt nog niet goed
+drinkbanner als overlay (flitsen?)
+*/
