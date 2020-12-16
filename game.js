a3e61ffbe5c83fa9af1bba3138030a0b346a3304
Rock'n'Rolo <42418205+RocknRolo@users.noreply.github.com>
2020-12-17 00:40:33 +0100

    het begint mee te werken..
    timer gefixt
    banner tonen / verbergen
    woord wordt goed uitgelezen
    vorig woord naar juiste plek

--- game.js
+++ game.js
@@ -4,64 +4,66 @@ let TIME_LIMIT = 10;
 // textbestand (csv xml json xml whatever) kan uitlezen.
 let wordList = ["hond", "vis", "slang", "giraffe", "egel", "slak", 
 "krekel", "lijster", "reiger", "rendier", "regenworm", "paard", "kat", 
-"tijger", "reebok","krokodil", "leeuw", "walvis", "schildpad", "dodo", "olifant", "teek"];
+"tijger", "reebok", "krokodil", "leeuw", "walvis", "schildpad", "dodo", 
+"olifant", "teek", "armadillo"];
 
 let namedWords = [];
-//console.log(wordList);
 
-let letter = document.querySelector(".letter");
-let timer = document.querySelector(".timer_text");
+let letter = document.querySelector(".letter_text");
+let timer_text = document.querySelector(".timer_text");
+let word_text = document.querySelector(".word_text");
 let input_area = document.querySelector(".input_area");
 let drink_banner = document.querySelector(".drink_banner");
 
 let timeLeft = TIME_LIMIT;
-let current_word = "";
+let timer = null;
 
-function processCurrentText() {
-
-    curr_input = input_area.value;
-    if (wordList.includes(curr_input)) {
-        current_word = curr_input;
-        curr_input = "";
-        resetValues;
+function startGame() {
+    if (timer == null) {
+        timer = setInterval(runTimer, 1000);
     }
 }
 
-function updateTimer() {
+function readText() {
+
+    if (wordList.includes(input_area.value)) {
+        current_word = input_area.value;
+        namedWords.push(current_word);
+        word_text.textContent = current_word;
+        input_area.value = "";
+        resetTimer();
+    }
+}
+
+function runTimer() {
     if (timeLeft > 0) {
         timeLeft--;
-        timer.textContent = timeLeft + "s";
+        timer_text.textContent = timeLeft + "s";
     }
     else {
         drink();
     }
 }
 
-function startGame() {
-
-    resetValues();
-
+function resetTimer() {
     clearInterval(timer);
-    timer_text = setInterval(updateTimer, 1000);
-}
-
-function resetValues() {
-    clearInterval(timer);
-
     timeLeft = TIME_LIMIT;
-    
-    input_area.value = "";
-    timer.textContent = timeLeft + 's';
+    timer_text.textContent = timeLeft + 's';
+    timer = setInterval(runTimer, 1000);
 }
 
 function drink() {
-    drink_banner.style.display = "block";
+    drink_banner.style.display = "block"; 
+    setTimeout(() => {  
+        drink_banner.style.display = "none";
+        resetTimer();
+    }, 2000);
 
 }
 
 /*
 TODO:
 list.txt inlezen als stringarray
-Timer werkt nog niet goed
-drinkbanner als overlay (flitsen?)
+(bug) timer pauseert na weergeven banner
+spelregels implementeren
 */
